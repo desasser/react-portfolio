d79610e487d7a1dc41de4fd5a69c68d8d1f8aa22
Daniel Sasser <desasser79@gmail.com>
2021-02-21 14:24:21 -0800

    project card built

--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -3,7 +3,9 @@ import React from 'react'
 export default function index() {
   return (
     <div>
-      
+      <ProjectTitle />
+      <ProjectTextContent />
+      <ProjectTech />
     </div>
   )
 }
